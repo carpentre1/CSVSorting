b3243bf9e6b8b50b5dc8ebeb764175e27ee59481
carpentre1 <jlowell.carpenter@gmail.com>
2019-09-16 14:42:34 -0500

    Preparations for next step

---
+++ InClass/InClass/SortingMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass
+{
+    /// <summary>
+    /// use this commenting structure for each method. two references for each sorting method
+    /// 8 total methods
+    /// read in a csv file as the input to your sorting methods, File.ReadLine to read one line of data at a time
+    /// <see cref="url"/>
+    /// /// <see cref="url"/>
+    /// </summary>
+    public static class SortingMethods
+    {
+        public static List<Guid> InsertionSort(List<Guid> theData)
+        {
+            return new List<Guid>();
+        }
+        public static List<Double> InsertionSort(List<Double> theData)
+        {
+            return new List<Double>();
+        }
+    }
+}
